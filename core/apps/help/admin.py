8bbdeefcc04a70455e6cfdbc05458bbe1cb8acb6
Evgeny Elamkov <GenyaElamkov@gmail.com>
2025-10-15 15:29:43 +0300

    Удалена таблица media в help

--- core/apps/help/admin.py
+++ core/apps/help/admin.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from .models.faq import FAQ
 from .models.help_article import HelpArticle
 from .models.help_category import HelpCategory
-from .models.help_media import HelpMedia
 
 
 @admin.register(HelpCategory)
@@ -15,14 +14,8 @@ class HelpCategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 
-class HelpMediaInline(admin.TabularInline):
-    model = HelpMedia
-    extra = 1
-
-
 @admin.register(HelpArticle)
 class HelpArticleAdmin(admin.ModelAdmin):
-    inlines = [HelpMediaInline]
     list_display = ['title', 'category', 'is_published', 'view_count']
     list_filter = ['is_published', 'category']
     search_fields = ['title', 'content', 'short_description']
