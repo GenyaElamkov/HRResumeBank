473db0b4c9c3fd0396605f201cce13ea3dadbe94
Evgeny Elamkov <GenyaElamkov@gmail.com>
2025-10-18 22:41:28 +0300

    Вынесены отделнь css и js

--- core/project/settings/main.py
+++ core/project/settings/main.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     'easyaudit.middleware.easyaudit.EasyAuditMiddleware',
