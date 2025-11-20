6d6198d574e40021ca3a9d288c682effd18be895
Evgeny Elamkov <GenyaElamkov@gmail.com>
2025-11-20 14:58:47 +0300

    Исправил .dockerignore Dockerfile.prod asgi.py

--- core/project/asgi.py
+++ core/project/asgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.project.settings.main")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.project.settings.prod")
 
 application = get_asgi_application()
