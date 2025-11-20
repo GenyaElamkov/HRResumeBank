ef5455a2cb43b29a1a7c277f170acf1a126fdbac
Evgeny Elamkov <GenyaElamkov@gmail.com>
2025-11-20 13:18:45 +0300

    feat: тест workflows

--- core/project/settings/test.py
+++ core/project/settings/test.py
@@ -1,3 +1,5 @@
+import sys
+
 from .main import *  # noqa
 
 
@@ -6,22 +8,20 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', 'main-app']
 
 # Используем SQLite для тестов
-# if 'test' in sys.argv or 'pytest' in sys.argv[0]:
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
-    },
-}
+if 'test' in sys.argv or 'pytest' in sys.argv[0]:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        },
+    }
 
+    # Отключаем миграции для ускорения тестов
+    class DisableMigrations:
+        def __contains__(self, item):
+            return True
 
-# Отключаем миграции для ускорения тестов
-class DisableMigrations:
-    def __contains__(self, item):
-        return True
+        def __getitem__(self, item):
+            return None
 
-    def __getitem__(self, item):
-        return None
-
-
-MIGRATION_MODULES = DisableMigrations()
+    MIGRATION_MODULES = DisableMigrations()
