61b48be0e9b0c1338c9ca16d3457f06f75c1d118
Evgeny Elamkov <GenyaElamkov@gmail.com>
2025-11-25 13:23:30 +0300

    Верул nginx в исходное

--- core/project/settings/main.py
+++ core/project/settings/main.py
@@ -7,8 +7,8 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 env = environ.Env()
-environ.Env.read_env(BASE_DIR / ".env.local")
-environ.Env.read_env(BASE_DIR / ".env.prod")
+# environ.Env.read_env(BASE_DIR / ".env.local") #noqa
+# environ.Env.read_env(BASE_DIR / ".env.prod")  #noqa
 
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
